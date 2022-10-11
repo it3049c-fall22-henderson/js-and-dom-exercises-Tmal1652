a0ca1cc9fbd04c8d4111b7825aafa56b19b93801
Tmal1652 <103225901+Tmal1652@users.noreply.github.com>
2022-10-10 20:27:08 -0400

    Password can be visable now

--- exercises/password-reveal/app.js
+++ exercises/password-reveal/app.js
@@ -3,7 +3,16 @@
 
 // 🤖: Create an Event Listener on the password reveal button
 // 🤖: The event listener should toggle the password field attribute between `password` and `text` to change the visibility of the password field content.
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
 
+togglePassword.addEventListener('click', function (e) {
+	// toggle the type attribute
+	const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+	password.setAttribute('type', type);
+	// toggle the eye / eye slash icon
+	this.classList.toggle('bi-eye');
+});
 
 // Provided for you - prevent form submit
 const form = document.querySelector("form");
